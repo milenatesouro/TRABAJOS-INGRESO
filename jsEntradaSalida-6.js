acbbd6a44406d6bb8625a041f272728f936a0b69
milenatesouro <40794852+milenatesouro@users.noreply.github.com>
2018-07-05 00:27:07 -0300

    Add files via upload

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()    {
+    var a;
+    var b;
+    var resultado;
+    numero a = getElementById ("numeroUno").value
+    numero b = getElementById ("numeroDos").value
+    a = "numeroUno";
+    b = "numeroDos";
+    resultado = a+b;
+    alert ("El valor del resultado es" + resultado );
+
+}
+
