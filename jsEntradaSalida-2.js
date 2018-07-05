195659a164b8b73ff14bd05b5e19900a72930618
milenatesouro <40794852+milenatesouro@users.noreply.github.com>
2018-07-04 22:34:58 -0300

    trabajos

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+    var dato;
+    dato = prompt ("Buenas tardes");
+    alert (dato);
+	
+}
+
